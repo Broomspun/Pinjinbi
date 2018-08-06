27c2b1933a4799bd3cc5b0e4ade78cdfba3fb927
broomspun1208 <admin@angelsolution.net>
2018-08-05 19:29:40 -0600

    Added NativeBase and linked

--- App.js
+++ App.js
@@ -8,33 +8,56 @@ import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
-  Text,
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { Container, Accordion, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+const dataArray = [
+    { title: "First Element", content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum" },
+    { title: "Second Element", content: "Lorem ipsum dolor sit amet" },
+    { title: "Third Element", content: "Lorem ipsum dolor sit amet" }
+];
+
 
 type Props = {};
 export default class App extends Component<Props> {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+      return (
+          <Container>
+              <Header>
+                  <Left>
+                      <Button transparent>
+                          <Icon name='menu' />
+                      </Button>
+                  </Left>
+                  <Body>
+                  <Title>Header123</Title>
+                  </Body>
+                  <Right />
+              </Header>
+              <Content>
+                  <Text>
+                      This is Content Section
+                  </Text>
+                  <Accordion
+                      dataArray={dataArray}
+                      headerStyle={{ backgroundColor: "#b7daf8" }}
+                      contentStyle={{ backgroundColor: "#ddecf8" }}
+                  />
+              </Content>
+              <Footer>
+                  <FooterTab>
+                      <Button full>
+                          <Text>Footer</Text>
+                      </Button>
+                  </FooterTab>
+              </Footer>
+          </Container>
+      );
   }
 }
 
